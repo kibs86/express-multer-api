f86f25c2ec76dfdb07ab314d5b3267f0da78f96a
kibs86 <kibs086@gmail.com>
2016-12-14 16:04:50 -0500

    Create new route and controller action
    - Create new route for /uploads
    - Create new controller action for uploads#create
    - Remove authentication since we don't need it
    - Add multer before action

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -3,6 +3,8 @@
 require('dotenv').load();
 
 const s3Upload = require('../lib/aws-s3-upload.js');
+const mongoose = require('../app/middleware/mongoose');
+const Upload = require('../app/models/upload');
 
 let file = {
   path: process.argv[2],
@@ -11,8 +13,19 @@ let file = {
 
 
 s3Upload(file)
-  .then(function(data){
+  .then(function(response){
     console.log("inside then block");
-    console.log("data is ", data);
+    console.log("data is ", response);
+    console.log("url is ", response.Location);
+    // Upload.create returns a promise so we can just return it here
+    return Upload.create({
+      url: response.Location,
+      // don't need a random title when we upload to mongo, only when we upload
+      // to AWS so we can use file.title here
+      title: file.title
+    });
   })
-  .catch(console.error);
+  .catch(console.error)
+  .then(function(){
+    mongoose.connection.close();
+  });
